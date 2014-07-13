6fe1ec93d03f09fbfddf083b6cb2dcd7155da294
Evan Coleman <evan@evancoleman.net>
2014-07-13 16:05:27 -0400

    Initial Commit

---
+++ Bikes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bikes
+//
+//  Created by Evan Coleman on 7/13/14.
+//  Copyright (c) 2014 Evan Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BKAppDelegate class]));
+    }
+}
