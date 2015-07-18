154148b3e1eb4b7e5e947d01b1896760b71a5aea
Evan Coleman <evan@evancoleman.net>
2015-07-18 15:27:03 -0400

    Initial swift project

---
+++ Bikes/Bikes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bikes
+//
+//  Created by Evan Coleman on 7/18/15.
+//  Copyright (c) 2015 Evan Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Gemfile
+++
@@ -1,3 +0,0 @@
-source "http://rubygems.org"
-
-gem "cocoapods", "~> 0.34.2"
