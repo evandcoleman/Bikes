680a191255397fbecf2c0bcac967aca1ccfd2fa4
Evan Coleman <evan@evancoleman.net>
2015-07-27 12:24:06 -0400

    Fix typo

--- Bikes/MainViewController.swift
+++ Bikes/MainViewController.swift
@@ -24,7 +24,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     }
 
-    // MARKL UITableViewDataSource
+    // MARK: UITableViewDataSource
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 2
